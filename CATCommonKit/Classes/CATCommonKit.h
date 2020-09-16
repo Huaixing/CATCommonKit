7edb1c15e1dd66210f0739a2251eb189225efe87
Shihuaixing <>
2020-09-16 18:23:15 +0800

    update

--- CATCommonKit/Classes/CATCommonKit.h
+++ CATCommonKit/Classes/CATCommonKit.h
@@ -9,7 +9,6 @@
 #define CATCommonKit_h
 
 #import <CATCommonKit/NSString+CATSize.h>
-#import <CATCommonKit/NSString+CATBundle.h>
 #import <CATCommonKit/UIView+CATSize.h>
 
 #endif /* CATCommonKit_h */
